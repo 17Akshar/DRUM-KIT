9918a13a27a7976c4da3a7c17fd50ee1aa80b867
17Akshar <aksharparmar2002@gmail.com>
2023-06-16 19:05:54 +0530

    mobile freindly audio

--- main.js
+++ main.js
@@ -9,4 +9,47 @@ window.addEventListener('keydown',function(e){
     key.classList.remove('playing')
   },100)
 })
-
+function kick(){
+  var audio = document.querySelector(`audio[data-key="65"]`);
+  var key = document.querySelector(`audio[data-key="65"]`);
+  if(!audio) return;
+  audio.currentTime = 0;  
+  audio.play();
+  key.classList.add('playing');
+  setTimeout(function(){
+    key.classList.remove('playing')
+  },100)
+}
+function clap(){
+  var audio = document.querySelector(`audio[data-key="83"]`);
+  var key = document.querySelector(`audio[data-key="83"]`);
+  if(!audio) return;
+  audio.currentTime = 0;  
+  audio.play();
+  key.classList.add('playing');
+  setTimeout(function(){
+    key.classList.remove('playing')
+  },100)
+}
+function hit(){
+  var audio = document.querySelector(`audio[data-key="68"]`);
+  var key = document.querySelector(`audio[data-key="68"]`);
+  if(!audio) return;
+  audio.currentTime = 0;  
+  audio.play();
+  key.classList.add('playing');
+  setTimeout(function(){
+    key.classList.remove('playing')
+  },100)
+}
+function snare(){
+  var audio = document.querySelector(`audio[data-key="70"]`);
+  var key = document.querySelector(`audio[data-key="70"]`);
+  if(!audio) return;
+  audio.currentTime = 0;  
+  audio.play();
+  key.classList.add('playing');
+  setTimeout(function(){
+    key.classList.remove('playing')
+  },100)
+}
